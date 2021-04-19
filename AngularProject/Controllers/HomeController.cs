6f43d6f4bd8e8123746969891ec061ffd7434b6d
inazarm <inazar.m@gmail.com>
2021-04-19 17:01:05 +0500

    complete application with crud operation in mvc using angular js

---
+++ AngularProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularProject.Models;
+
+namespace AngularProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult getrecord() 
+        {
+            using (ResearchDBEntities db=new ResearchDBEntities())
+            {
+                var listreg = db.tbl_Status.ToList();
+                return Json(listreg, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
---
+++ AngularProject/Controllers/TestController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularProject.Models;
+
+namespace AngularProject.Controllers
+{
+    public class TestController : Controller
+    {
+        MvcCRUDDBEntities entities;
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetCustomers()
+        {
+            entities = new MvcCRUDDBEntities();
+            return Json(entities.Customers);
+        }
+
+        [HttpPost]
+        public JsonResult InsertCustomer(Customer customer)
+        {
+            using (entities = new MvcCRUDDBEntities())
+            {
+                entities.Customers.Add(customer);
+                entities.SaveChanges();
+            }
+
+            return Json(customer);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCustomer(Customer customer)
+        {
+            using (entities = new MvcCRUDDBEntities())
+            {
+                Customer updatedCustomer = (from c in entities.Customers
+                                            where c.CustomerId == customer.CustomerId
+                                            select c).FirstOrDefault();
+                updatedCustomer.Name = customer.Name;
+                updatedCustomer.Country = customer.Country;
+                entities.SaveChanges();
+            }
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCustomer(int customerId)
+        {
+            using (entities = new MvcCRUDDBEntities())
+            {
+                Customer customer = (from c in entities.Customers
+                                     where c.CustomerId == customerId
+                                     select c).FirstOrDefault();
+                entities.Customers.Remove(customer);
+                entities.SaveChanges();
+            }
+            return new EmptyResult();
+        }
+    }
+}
